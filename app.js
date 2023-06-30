0720eaa5364d5370fc6156e623303cf1c57efd07
seth-stitik <87721353+sstickk@users.noreply.github.com>
2023-06-30 13:57:33 -0300

    Update app.js
    Hid OpeanWeatherMap API key

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ console.log("Made by Seth Stitik (:");
 console.log("Don't worry, your location information is not stored or viewed!");
 
 let weather = {
-    apikey: "7950f51d76eb6d0a68ae57e95c9bda5b",
+    apikey: process.env.OPENWEATHERMAP_API_KEY,
     units: {
         imperial: {
             temperature: "imperial",
